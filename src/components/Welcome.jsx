fbfecb3109925d017f1e51f8a1bcc7eac90f18f0
Steve Wang <stevewang347@gmail.com>
2025-11-22 16:43:57 -0500

    implement coderabbit changes

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -20,7 +20,7 @@ const renderText = (text, className, baseWeight = 400) =>{
 }
 
 const setupTextHover = (container, type) =>  {
-    if(!container) return;
+    if(!container) return () => {};
 
     const letters = container.querySelectorAll("span");
     const {min, max, default: base} = FONT_WEIGHTS[type];
@@ -88,7 +88,7 @@ function Welcome() {
     </section>;
 
     <div className="small-screen">
-        <p>This Portfoio is designed for desktop/tabled screens only.</p>
+        <p>This Portfoio is designed for desktop/tablet screens only.</p>
     </div>
 };
 
